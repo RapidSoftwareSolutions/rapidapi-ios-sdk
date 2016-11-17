4b22afb46936a4f71cf73c3470aa6792cde4e5c1
Andrey Bukati <andrey@bukati.me>
2016-11-17 18:58:30 +0200

    Framework file added

---
+++ Framework/RapidAPISDK.framework/Headers/RapidAPISDK.h
@@ -0,0 +1,21 @@
+//
+//  RapidConnectSDK.h
+//  RapidConnectSDK
+//
+//  Created by Andrey Bukati on 20/10/2016.
+//  Copyright © 2016 RapidAPI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RapidConnectSDK.
+FOUNDATION_EXPORT double RapidConnectSDKVersionNumber;
+
+//! Project version string for RapidConnectSDK.
+FOUNDATION_EXPORT const unsigned char RapidConnectSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RapidConnectSDK/PublicHeader.h>
+
+#import <RapidAPISDK/RapidAPI.h>
+
+
